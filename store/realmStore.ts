9f731efc8cf43d26c888179121d15225ab9e6409
Pallav K <furinehigh@outlook.com>
2025-12-21 16:45:57 +0530

    added proper types and store variables for player and controls

--- store/realmStore.ts
+++ store/realmStore.ts
@@ -4,6 +4,8 @@ import { create } from "zustand";
 
 export const useRealmStore = create<RealmState>((set) => ({
     riftOpen: false,
+    showMobileControls: true,
+    toggleControls: () => set(s=> ({showMobileControls: !s.showMobileControls})),
     toggleRift: () => set(s => ({ riftOpen: !s.riftOpen })),
     openRift: () => set({ riftOpen: true }),
     closeRift: () => set({ riftOpen: false })
--- types/types.ts
+++ types/types.ts
@@ -11,7 +11,30 @@ export type RealmConfig = {
 
 export type RealmState = {
     riftOpen: boolean
+    showMobileControls: boolean
     toggleRift: () => void
     openRift: () => void
     closeRift: () => void
+    toggleControls: () => void
 }
+
+export type PlayerState = {
+    x: number
+    y: number
+    vx: number
+    vy: number
+    isGrounded: boolean
+    isDead: boolean
+    facingRight: boolean
+    realm: 'normal' | 'shadow'
+    lastRiftSwitch: number
+}
+
+export type Platform = {
+    id: string
+    x: number
+    y: number
+    width: number
+    height: number
+}
+
